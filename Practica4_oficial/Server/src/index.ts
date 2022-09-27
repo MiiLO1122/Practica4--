95460030f8dbdbcb36ca91080dfbd88cce09d786
Andrés Emilio Peñate <3428938822207@ingenieria.usac.edu.gt>
2022-09-26 23:19:59 -0600

    discard

--- Practica4_oficial/Server/src/index.ts
+++ Practica4_oficial/Server/src/index.ts
@@ -3,9 +3,24 @@ import express, {Application} from 'express';
 class server{
 
     public app: Application;
-     
+
     constructor(){
         this.app = express();
+    }
+
+    config(): void{
+        //puerto
+        this.app.set('port',process.env.PORT || 3000);
+    }
+    //rutas
+    routes():void{
+
+    }
+
+    //inicia server
+    start(): void{
+        this.app.listen();
+        console.groupCollapsed('ejecutando en puero',this.app.get('PORT'))
     }
 }
 
