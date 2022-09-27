a79012608d38f689027e317e9c3d5dffd5148b9c
Singrid Martinez <3403252160101@ingenieria.usac.edu.gt>
2022-09-27 00:17:14 -0600

    Commit

--- Practica 4/server/src/keys.ts
+++ Practica 4/server/src/keys.ts
@@ -2,10 +2,11 @@
 export default {
 
     database: {
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "masctoas"
+        host: "btowwqgmhdu4cl3fjyew-mysql.services.clever-cloud.com",
+        user: "ufiwgjimeexxkopm",
+        password: "EkYvvfdh2FFnFLOoiDZ4",
+        database: "btowwqgmhdu4cl3fjyew",
+        port: 3306
     }
 
 }
